f684900277364551c04cdf08483e0f5c05608afa
Mika Pons <mika.pons@irit.fr>
2023-10-12 11:55:11 +0200

    [modified] src/fizzbuzz/FizzBuzz.java

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz {
 	public static Object fizzbuzz(int i) {
 		String resultat = "";
 		if (i%3==0)
-			return "fizz";
+			resultat.concat("fizz");
 		if (i%5==0)
 			return "buzz";
 		return Integer.toString(i);
