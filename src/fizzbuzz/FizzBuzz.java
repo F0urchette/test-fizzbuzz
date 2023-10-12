2771f27ef108a0dec170a4385f74bfb4df244b46
Mika Pons <mika.pons@irit.fr>
2023-10-12 11:49:42 +0200

    [modified] src/fizzbuzz/FizzBuzz.java

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,9 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	public static Object fizzbuzz(int i) {
-		if (i==3)
+		if (i%3)
 			return "fizz";
-		if (i==5)
+		if (i%5)
 			return "buzz";
 		return Integer.toString(i);
 	}
