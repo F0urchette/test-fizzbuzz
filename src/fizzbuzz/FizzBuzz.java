1c06a44ed6d1351832d150f422e37b286e4dd703
Mika Pons <mika.pons@irit.fr>
2023-10-12 11:57:23 +0200

    [modified] src/fizzbuzz/FizzBuzz.java

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,10 @@ package fizzbuzz;
 public class FizzBuzz {
 	public static Object fizzbuzz(int i) {
 		String resultat = "";
-		System.out.println(resultat.length());
 		if (i%3==0)
-			resultat.concat("fizz");
+			resultat = resultat.concat("fizz");
 		if (i%5==0)
-			resultat.concat("buzz");
+			resultat = resultat.concat("buzz");
 		if (resultat.isEmpty())
 			return Integer.toString(i);
 		return resultat;
